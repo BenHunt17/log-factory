860e6b629f8ac6333e987e81b449ee125733b0ee
BenHunt17 <58478479+BenHunt17@users.noreply.github.com>
2025-03-02 17:20:07 +0000

    Added draft UI

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,10 +1,41 @@
-import { Link } from "expo-router";
-import { View, Text, StyleSheet } from "react-native";
+import { colors } from "@/theme/colors";
+import { View, Text, Button, StyleSheet } from "react-native";
+import {
+  GestureHandlerRootView,
+  ScrollView,
+} from "react-native-gesture-handler";
+
+const fileNames = ["test.md", "test2.md", "gym.md"];
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <GestureHandlerRootView>
+      <ScrollView style={styles.root}>
+        <View style={styles.layout}>
+          <Text style={styles.heading}>Select schema</Text>
+          {fileNames.map((x) => (
+            <Button key={x} onPress={() => {}} title={x} />
+          ))}
+        </View>
+      </ScrollView>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: colors.light.background,
+  },
+  layout: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 16,
+    paddingVertical: 16,
+    paddingHorizontal: 8,
+  },
+  heading: {
+    color: colors.light.text,
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
